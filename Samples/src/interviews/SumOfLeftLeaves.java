fab16b578e37c86a9ef4894820863c94d47d783f
cptspock <dreamday@gmail.com>
2019-04-27 21:49:15 -0700

    added

---
+++ Samples/src/interviews/SumOfLeftLeaves.java
@@ -0,0 +1,34 @@
+package interviews;
+
+/**
+ * 
+	    3
+	   / \
+	  9  20
+	    /  \
+	   15   7
+
+	There are two left leaves in the binary tree, with values 9 and 15 respectively. Return 24.
+ *
+ */
+
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+}
+
+public class SumOfLeftLeaves {
+
+	public int sumOfLeftLeaves(TreeNode root) {
+		if (root == null) return 0;
+		else if (root.left != null && root.left.left == null && root.left.right == null) {
+			return root.left.val + sumOfLeftLeaves(root.right);
+		}
+		else return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
+	}
+}
