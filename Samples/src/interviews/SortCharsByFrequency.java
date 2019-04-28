dfda3c6c0eff59a423abda604d9e5cbb6478f43b
cptspock <dreamday@gmail.com>
2019-04-27 20:10:04 -0700

    added

--- Samples/src/interviews/SortCharsByFrequency.java
+++ Samples/src/interviews/SortCharsByFrequency.java
@@ -4,6 +4,22 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.PriorityQueue;
 
+/**
+ * Input:
+ * "tree"
+ *
+ * Output:
+ * "eert"
+ *
+ * Input:
+ * "cccaaa"
+ *
+ * Output:
+ * "cccaaa"
+ * 
+ * Order is not important if there are 2 characters with same count.
+ */
+	
 public class SortCharsByFrequency {
 
 	public String frequencySort(String s) {
@@ -13,6 +29,7 @@ public class SortCharsByFrequency {
 			charMap.put(c, charMap.getOrDefault(c, 0) + 1);
 		}
 		
+		// create PQ using lambda which specifies to add keys in descending order of the values
 		PriorityQueue<Character> maxHeap = new PriorityQueue<>((a, b) -> charMap.get(b) - charMap.get(a));
 		maxHeap.addAll(charMap.keySet());
 		
